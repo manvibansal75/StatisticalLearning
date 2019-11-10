22dec6791df71a2109c9087594b41abf012db566
manvibansal75 <33548808+manvibansal75@users.noreply.github.com>
2019-11-10 12:52:35 +0800

    Add files via upload
    Data preprcoessing - Getting all files as a data frame

---
+++ Stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 10 11:52:35 2019
+
+@author: manvibansal
+"""
+
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import numpy as np
+import random
+import glob
+
+#dataset=pd.read_csv('./StatsData/Kickstarter_2019/*.csv')
+
+
+
+path = './StatsData/Kickstarter_2019'# use your path
+all_files = glob.glob(path + "/*.csv")
+
+dataset = []
+
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=0)
+    dataset.append(df)
+
+frame = pd.concat(dataset, axis=0, ignore_index=True)
+
